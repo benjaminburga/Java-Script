a8e3c3ff4dc8f0a989feefbec39cb755cf95dd84
benjaminburga <80055813+benjaminburga@users.noreply.github.com>
2023-03-23 22:43:35 -0500

    Update index.js

--- Leccion 2/index.js
+++ Leccion 2/index.js
@@ -1,7 +1,7 @@
 
 
-const nombre = "Benjamin"
-const edad= 23
+const Nombre = "Benjamin"
+const Edad= 23
 const EresDesarrollador = true
 const FechaDeNacimiento = new Date ("Octuber 29 1999")
 const LibroFavorito= ["Cartas a Magdalena" , "Jose Luis Mejia" , 1971 ,"https://www.goodreads.com/book/show/26202030-cartas-a-mar-a-elena"]
